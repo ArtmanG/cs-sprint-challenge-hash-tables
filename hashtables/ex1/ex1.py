bb590cee4928b3d1b37144a4f9b9882c393cdd7e
Gregory Hawman <g.hawman@gmail.com>
2020-08-07 10:41:52 -0700

    got ex1 working

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,23 @@
 def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    # create a cache
+    cache = {}
 
-    return None
+    if len(weights) < 2:
+        return None
+
+    # index of weights stores as values in cache, start at index 0 with index counter
+    index = 0
+    # loop through array of weights
+    for weight in weights:
+        # look for (limit - weight) in cache
+        if (limit - weight) in cache:
+            # if found return the index we are on, the index of the other weight 
+            return(index, cache[(limit - weight)])
+        #if not in cache, add the weight:index (key:value) pair to the cache
+        cache[weight] = index
+        # move on to next index
+        index += 1
+
+# testing for myself
+weights_4 = [12, 6, 7, 14, 19, 3, 0, 25, 40]
+get_indices_of_item_weights(weights_4, 9, 7)
